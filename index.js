86ba0d6e6d7e78883204c1df69d5d7989379dce3
USAMAWIZARD <usamawizard@gmail.com>
2020-07-18 10:39:38 +0530

    taking camara input passing it to model  returing the key point and detecting jumps

---
+++ index.js
@@ -0,0 +1,53 @@
+
+app=require("express");
+img=require("capture-video-frame")
+var uname
+require("ejs");
+server=app();
+var ser = server.listen(5000)
+var io = require('socket.io').listen(ser);
+
+session = require("express-session")({
+  secret: "my-secret",
+  resave: true,
+  saveUninitialized: true
+})
+var Peer = require('simple-peer')    
+var cookieParser = require('cookie-parser')
+server.use(cookieParser())
+ 
+server.use(session);   //ses for managing users
+
+sharedsession = require("express-socket.io-session");
+io.use(sharedsession(session));
+
+//Set View Engine To EJS
+server.set("view engine", "ejs");
+//Set Static Directory
+server.use(app.static(__dirname));
+const bodyParser= require('body-parser');
+server.use(bodyParser.urlencoded({extended:true}))
+const url = require('url');
+const { render } = require("ejs");
+const { exception } = require("console");
+const { maxHeaderSize } = require("http");
+const { Socket } = require("net");
+server.use(bodyParser.json());
+var socid
+//mongodb
+MongoClient = require('mongodb').MongoClient,
+server.get('/', function(req, res) {  res.render('steam');});
+io.on ("connection",socket=>{
+  socket.emit("sendfeed",'you are connected')
+
+socket.on('data',function(data){
+  console.log('andar aaya')
+  socket.broadcast.emit('send',data);  
+});
+socket.on('makeconn',function(data){
+  socket.broadcast.emit('conn',data)
+})
+});
+server.get('/recive',function(res,res){
+res.render('reciver')
+})
---
+++ script.js
@@ -0,0 +1,16 @@
+var socket = io.connect();
+
+socket.on('sendfeed',hello=>{
+    console.log(socket.id)
+}); 
+socket.on('send',data=>{
+    console.log('uska datas')
+    console.log(data)
+    peer.signal(data)
+
+});
+
+socket.on('conn',data=>{
+    console.log('connection ')
+    peer.signal(data)
+})
